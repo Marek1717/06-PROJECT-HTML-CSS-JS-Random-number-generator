4a310b097dcbc6cad719b44bed74dda453314bff
Marek1717 <marekpisarczyk20@gmail.com>
2021-05-19 19:22:42 +0200

    06 PROJECT - HTML CSS JS Random number generator

---
+++ script.js
@@ -0,0 +1,8 @@
+const number = document.querySelector(".number");
+const btn = document.querySelector(".btn");
+
+const generateNumber = () => {
+    const randomNumber = Math.floor(Math.random() * 10 + 1);
+    number.innerHTML = randomNumber;
+}
+btn.addEventListener("click", generateNumber);
